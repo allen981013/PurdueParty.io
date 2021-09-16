26a62db27ac14f64a2681da19de1fd04b90c208e
Raziq Raif Ramli <42981908+raziqraif@users.noreply.github.com>
2021-09-16 16:01:05 -0400

    Initial navbar implementation

---
+++ src/components/navbar/NavBar.tsx
@@ -0,0 +1,52 @@
+import './NavBar.css'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { RootState } from '../../store/reducers';
+
+interface NavBarProps {
+}
+
+interface NavBarState {
+    isLoggedIn?: boolean;
+    userName?: string;
+}
+
+class NavBar extends Component<NavBarProps, NavBarState> {
+  render() {
+    return (
+      <div id='topbar'>
+          <div id='topbar__gold'>
+            <div>
+              <a href="/signin">Hello, Guest</a> 
+              <a href="/signin">Sign In</a>
+            </div>
+          </div>
+          <div id="topbar__black">
+          </div>
+          <div id="topbar__nav">
+            <div>
+              <a href="/">Home</a>
+              <a href="/events">Events</a>
+              <a href="/market">Market</a>
+              <a href="/life">Life</a>
+              <a href="/classes">Classes</a>
+              <a href="/club">Clubs</a>
+            </div>
+         </div>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state: RootState) => {
+  return {
+  }
+}
+
+const mapDispatchToProps = (dispatch: any) => {
+  // Return functions from actions folder
+  return {
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavBar);
