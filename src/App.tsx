a72982b2508480c804cf50702a007ae5c230a134
Raziq Raif Ramli <42981908+raziqraif@users.noreply.github.com>
2021-09-16 16:01:38 -0400

    Add navbar to app

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import SignIn from './components/auth/SignIn';
 import Events from './components/events/Events';
+import NavBar from './components/navbar/NavBar'
 // import Navbar 
 
 
@@ -10,9 +11,11 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <NavBar/>
         <Switch>
-          <Route exact path = '/' component = {SignIn} />
+          <Route exact path = '/' component = {Events} />
           <Route path = '/events' component = {Events} />
+          <Route path = '/signin' component = {SignIn} />
         </Switch>
       </div>
     </BrowserRouter>
