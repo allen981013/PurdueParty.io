a3bee41e97ff5c9e3d5d8160bd1d71aed7de9b8b
Raziq Ramli <42981908+raziqraif@users.noreply.github.com>
2021-09-22 23:21:09 -0400

    Integrate redux stuff to landing page

--- src/components/landing/Landing.tsx
+++ src/components/landing/Landing.tsx
@@ -1,11 +1,16 @@
 import './Landing.css'
 import React, {Component} from 'react'
-import Container from '@mui/material/Container'
-import Box from '@mui/material/Box'
-import Grid from '@mui/material/Grid'
-import Item from '@mui/material/Grid'
+import { Box, Grid, Button } from '@mui/material'
+import { LandingStatesRedux, loadLandingPageContent } from './LandingSlice'
+import { AppDispatch, RootState } from '../../store'
+import { connect } from 'react-redux'
 
 interface LandingProps {
+  events: LandingStatesRedux["events"];
+  saleItems: LandingStatesRedux["saleItems"];
+  classes: LandingStatesRedux["classes"];
+  clubs: LandingStatesRedux["clubs"];
+  loadLandingPageContent: () => void;
 }
 
 interface LandingStates {
@@ -18,41 +23,9 @@ interface LandingStates {
 
 class Landing extends Component<LandingProps, LandingStates> {
 
-  events = [
-    {title:"Bowling Day", href:"events/1", time:"10.00am"}, 
-    {title:"Frisbee Game", href:"events/2", time:"12.00pm"}, 
-    {title:"Hello World Callout", href:"events/3", time:"4.00pm"}, 
-    {title:"Movie Night", href:"events/4", time:"8.00pm"}, 
-    {title:"Theatre Club Callout", href:"events/5", time:"12/12"}, 
-  ]
-  saleItems = [
-    {title:"Ironing board", href:"marketplace/1", price:"$12"}, 
-    {title:"Webcam", href:"marketplace/2", price:"$30"}, 
-    {title:"Coffee Table", href:"events/3", price:"$27"}, 
-    {title:"Bike", href:"marketplace/4", price:"$49"}, 
-    {title:"Succulent", href:"marketplace/5", price:"$15"}, 
-  ]
-  classes = [
-    {title:"CS 407: Software Engineering", href:"class/CS407"}, 
-    {title:"CS 426: Computer Security", href:"class/CS426"}, 
-    {title:"CS 426: Microeconomics", href:"class/ECON251"}, 
-    {title:"STAT 512: Applied Regression Analysis", href:"class/STAT512"}, 
-    {title:"MA 453: Abstract Algebra", href:"class/MA453"}, 
-  ]
-  clubs = [
-    {title:"Archery Club", href:"clubs/1"}, 
-    {title:"Asian American Association", href:"clubs/2"}, 
-    {title:"Purdue Hackers", href:"clubs/3"}, 
-    {title:"Actuarial Science Club", href:"clubs/4"}, 
-    {title:"Hack The Future", href:"clubs/5"}, 
-  ]
-  eventCards = this.events.map((event) => this.getItemCard(event.title, event.href, event.time))
-  saleItemCards = this.saleItems.map((item) => this.getItemCard(item.title, item.href, item.price))
-  classCards = this.classes.map((class_) => this.getItemCard(class_.title, class_.href))
-  clubCards = this.clubs.map((club) => this.getItemCard(club.title, club.href))
-  
   constructor(props: LandingProps) {
-    super(props)
+    super(props);
+    this.props.loadLandingPageContent()
   }
 
   getItemCard(title: string, href: string, detail?: string) {
@@ -85,20 +58,48 @@ class Landing extends Component<LandingProps, LandingStates> {
       </Grid>
     )
   }
-
+    
   render() {
     return (
       <Box sx={{display: "flex", flexDirection: "column", alignItems: "center", flexGrow: 1}}>
           <Box id="cropped-purdue-img"/>
           <Grid container className="sections" spacing={2} sx={{padding: "32px 16px"}}>
-            {this.getSectionCard("Events", this.eventCards)}
-            {this.getSectionCard("Marketplace", this.saleItemCards)}
-            {this.getSectionCard("Classes", this.classCards)}
-            {this.getSectionCard("Clubs", this.clubCards)}
+            {
+              this.getSectionCard("Events", 
+              this.props.events.map((event) => this.getItemCard(event.title, event.href, event.time)))
+            }
+            {
+              this.getSectionCard("Marketplace", 
+              this.props.saleItems.map((item) => this.getItemCard(item.title, item.href, item.price)))
+            }
+            {
+              this.getSectionCard("Classes", 
+              this.props.classes.map((class_) => this.getItemCard(class_.title, class_.href)))
+            }
+            {
+              this.getSectionCard("Clubs", 
+              this.props.clubs.map((club) => this.getItemCard(club.title, club.href)))
+            }
           </Grid>
       </Box>
     )
   }
 }
 
-export default Landing;
+const mapStateToProps = (state: RootState) => {
+  return {
+    events: state.landing.events,
+    saleItems: state.landing.saleItems,
+    classes: state.landing.classes,
+    clubs: state.landing.clubs,
+  }
+}
+
+const mapDispatchToProps = (dispatch: AppDispatch) => {
+  // Insert functions from actions folder in similar syntax
+  return {
+    loadLandingPageContent: () => dispatch(loadLandingPageContent()),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Landing);
