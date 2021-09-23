4c5c30e2a28a13e83807c811bfb0d9f837fe90e2
Matt Story <matthewstory57@gmail.com>
2021-09-23 14:23:35 -0400

    Implement "Wait For Auth"
    Implemented a feature such that each page waits for authentication before it renders

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import SignIn from './components/auth/SignIn';
+import Homepage from './components/homepage/Homepage';
 import Events from './components/events/Events';
 // import Navbar 
 
@@ -11,8 +12,9 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <Switch>
-          <Route exact path = '/' component = {SignIn} />
+          <Route exact path = '/' component = {Homepage} />
           <Route path = '/events' component = {Events} />
+          <Route path = '/signin' component = {SignIn} />
         </Switch>
       </div>
     </BrowserRouter>
