dabb57276719f8ff8487e2e80bd189b0d8d7a083
Bryan Wang <wang3018@purdue.edu>
2021-10-04 13:54:51 -0400

    Added basic create-class-post functionality. Will have to refactor once class forums are created

---
+++ src/store/reducers/postReducer.tsx
@@ -0,0 +1,36 @@
+import { Timestamp } from '@firebase/firestore';
+
+// Initial state/dummy data, this gets replaced by Firebase info
+const initState = {
+    posts: [{
+        postId: "",
+        owner: "",
+        classID: "",
+        title: "",
+        description: "",
+        postedDateTime: new Timestamp(0,0),
+        upvotes: 1,
+        downvotes: 0,
+        comments: []
+    }]
+};
+
+type Action = {
+    type: string,
+    payload?: any, // Annotate the payload with proper type, if there are any
+}
+
+const postReducer = (state=initState, action: Action) => {
+    switch (action.type) {
+        case 'ADD_POST_SUCCESS':
+            console.log('added new post');
+            return state;
+        case 'ADD_POST_ERR':
+            console.log('error adding new post');
+            return state;
+        default :
+            return state;
+    }
+}
+
+export default postReducer;
