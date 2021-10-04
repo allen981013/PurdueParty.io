de68d34b5b33f8864c540068bf8c30fc3a99056c
Raziq Raif Ramli <42981908+raziqraif@users.noreply.github.com>
2021-10-04 07:45:51 -0400

    Create initial fetch events backend

--- src/components/events/EventsLandingSlice.ts
+++ src/components/events/EventsLandingSlice.ts
@@ -4,6 +4,7 @@ import { Timestamp } from 'firebase/firestore'
 import { Description, TagSharp } from '@mui/icons-material'
 import { GetDerivedStateFromError } from 'react'
 import { RootState } from '../../store'
+import { firebaseStorageRef } from '../..'
 
 // type for states returned by reducer
 export interface EventsLandingStatesRedux {
@@ -12,7 +13,8 @@ export interface EventsLandingStatesRedux {
     startTime: string,
     location: string,
     imageUrl: string,
-    href: string
+    href: string,
+    hostName: string
   }[];
   isEventsFetched: boolean;
   isLastPage: boolean;
@@ -21,13 +23,6 @@ export interface EventsLandingStatesRedux {
 // initial states
 const initState: EventsLandingStatesRedux = {
   events: [],
-  // {
-  //   title: "My Event",
-  //   startTime: new Date().toLocaleString("en-US"),
-  //   location: "Purdue University",
-  //   imageUrl: "https://firebasestorage.googleapis.com/v0/b/purdueparty-44444.appspot.com/o/events%2Fdefault.jpg?alt=media&token=1b1656e4-8f07-4f31-8f29-49a5f50bbe7c",
-  //   href: "/events"
-  // },
   isEventsFetched: false,
   isLastPage: true,
 }
@@ -56,86 +51,50 @@ export const eventsLandingSlice = createSlice({
   },
 })
 
-export const fetchEventsAtNextPage = () => {
-  return async (dispatch: Dispatch<Action>, getState: () => RootState, { getFirebase, getFirestore }: any) => {
-    const state = getState()
-    const db = getFirestore()
-    console.log({state})
-    // var eventsQueryPromise = db.collection("events")
-    //   .where("startTime", ">", Timestamp.now())
-    //   .orderBy("startTime").limit(9)
-    //   .get().then((querySnapshot: any) => {
-    //     querySnapshot.forEach((doc: any) => {
-    //     }
-    //   }
-  }
-}
+// actions 
 
-// actions
-export const fetchEventInfo = (eventID: string) => {
-  /**
-   * Read db content for event information and populate the store through the reducer.
-   */
-  return async (dispatch: Dispatch<Action>, getState: any, { getFirebase, getFirestore }: any) => {
+/* Uncomment this if we're storing image paths instead of image url in firestore */
+export const fetchEvents = (furthestPage: number) => {
+  return async (dispatch: Dispatch<Action>, getState: () => RootState, { getFirebase, getFirestore }: any) => {
     const db = getFirestore()
-    // Query event info
-    // db.collection("events").doc(eventID).get()
-    //   .then((doc: any) => {
-    //     // Return if event not found
-    //     if (doc.data() === undefined) {
-    //       dispatch(eventInfoSlice.actions.eventsNotFound())
-    //       return
-    //     }
-    //     // Map event info
-    //     var title = doc.data().title
-    //     var startTime = doc.data().startTime.toDate().toLocaleString("en-US")
-    //     var endTime = doc.data().endTime.toDate().toLocaleString("en-US")
-    //     var location = doc.data().location
-    //     var description = doc.data().description
-    //     var categories = doc.data().categories
-    //     var perks = doc.data().perks
-    //     var imageUrl = doc.data().imageUrl
-    //     var hostID = doc.data().hostID
-    //     var hostType = doc.data().hostType
-    //     var hostName: string | undefined = undefined
-    //     var hostHref: string | undefined = undefined
-    //     // Query host info
-    //     if (hostType == "USER") {
-    //       var hostQueryPromise = db.collection("users").doc(hostID).get()
-    //         .then((userDoc: any) => {
-    //           // Map host info 
-    //           hostName = userDoc.data().userName
-    //           hostHref = "/users/" + hostName
-    //         })
-    //     }
-    //     else if (hostType == "CLUB") {
-    //       // TODO: Finish this once club info has been created
-    //     }
-    //     // Create payload and dispatch
-    //   //   Promise.all([hostQueryPromise])
-    //   //     .then(() => {
-    //   //       type PayloadType = {
-    //   //         events: EventsLandingStatesRedux["events"],
-    //   //       }
-    //   //       const payload = {
-    //   //         event: {
-    //   //           title: title,
-    //   //           startTime: startTime,
-    //   //           endTime: endTime,
-    //   //           location: location,
-    //   //           description: description,
-    //   //           tags: categories,
-    //   //           imageUrl: imageUrl,
-    //   //           perks: perks,
-    //   //           categories: categories,
-    //   //         },
-    //   //         host: {
-    //   //           name: hostName,
-    //   //           href: hostHref,
-    //   //         }
-    //   //       }
-    //   //       dispatch(eventInfoSlice.actions.eventsFetched(payload))
-    //   //     });
-    //   });
+    const itemsPerPage = 12
+    // Query & populate events
+    var queryEventsPromise = db.collection("events")
+      .where("startTime", ">", Timestamp.now())
+      .orderBy("startTime")
+      .limit(furthestPage * itemsPerPage)
+      .get()
+    var createEventPromisesPromise = queryEventsPromise.then((querySnapshot: any) => {
+      return querySnapshot.docs.map((doc: any) => {
+        let queryUrlPromise = firebaseStorageRef.child(doc.data().imagePath).getDownloadURL()
+        if (doc.data().ownerID.length > 0) {
+          var queryHostPromise = db.collection("users").doc(doc.data().ownerID).get()
+        }
+        else if (doc.data().ownerID.length > 0) {
+          // TODO: Query when host is a club when we've reached that user story
+        }
+        let createEventPromise = Promise.all([queryUrlPromise, queryHostPromise])
+          .then((urlAndHostTuple) => {
+            return {
+              title: doc.data().title,
+              startTime: doc.data().startTime.toDate().toLocaleString("en-US"),
+              location: doc.data().location,
+              imageUrl: urlAndHostTuple[0],
+              href: "/events/" + doc.data().id,
+              hostName: urlAndHostTuple[1].data().userName, // TODO: Cater to the case when the host is a club entity
+            }
+          })
+        return createEventPromise
+      })
+    })
+    createEventPromisesPromise.then((createEventPromises: any[]) => {
+      Promise.all(createEventPromises).then((events: any) => {
+        let payload = {
+          events: events,
+          isLastPage: events.length < furthestPage * itemsPerPage,
+        }
+        dispatch(eventsLandingSlice.actions.eventsFetched(payload))
+      })
+    })
   }
 }
