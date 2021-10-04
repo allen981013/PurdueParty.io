b853109b1fd2d43ed3d01b7d24ac78be280b5011
Raziq Raif Ramli <42981908+raziqraif@users.noreply.github.com>
2021-10-04 07:46:25 -0400

    Create events landing page

--- src/components/events/EventsLanding.tsx
+++ src/components/events/EventsLanding.tsx
@@ -1,15 +1,18 @@
-import { Box, Button, CircularProgress, Grid } from '@mui/material'
+import {
+  Box, Button, CircularProgress, Grid, Card, CardActionArea,
+  CardMedia, CardContent, Typography
+} from '@mui/material'
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { AppDispatch, RootState } from '../../store'
-import { EventsLandingStatesRedux, fetchEventsAtNextPage } from './EventsLandingSlice'
+import { EventsLandingStatesRedux, fetchEvents } from './EventsLandingSlice'
 
 interface EventsLandingProps {
   events: EventsLandingStatesRedux["events"];
   isEventsFetched: boolean;
   isLastPage: boolean;
-  fetchEventsAtNextPage: () => void;
+  fetchEvents: (furthestPage: number) => void;
 }
 
 interface EventsLandingStates {
@@ -17,28 +20,57 @@ interface EventsLandingStates {
 
 
 class EventsLanding extends React.Component<EventsLandingProps, EventsLandingStates> {
+  furthestPage = 1
 
   constructor(props: EventsLandingProps) {
     super(props)
-
   }
 
   componentDidMount() {
     console.log("")
+    this.props.fetchEvents(this.furthestPage)
   }
 
-  getCards(events: any[]) {
-    return <Grid
-      item
-      id="image-container"
-      xs={12}
-      md={6}
-    >
-    </Grid>
+  getCards(events: EventsLandingStatesRedux['events']) {
+    return events.map((event) =>
+      <Grid
+        item
+        id="image-container"
+        xs={12}
+        md={3}
+      >
+        <Card>
+          <CardActionArea component={Link} to={event.href}>
+            <CardMedia
+              component="img"
+              height="140"
+              image={event.imageUrl}
+            />
+            <CardContent sx={{ display: "flex", flexDirection: "column", alignItems: "flex-start" }}>
+              <Typography gutterBottom noWrap component="div">
+                {event.title}
+              </Typography>
+              <Typography noWrap variant="body2" color="text.secondary" component="div">
+                {event.startTime}
+              </Typography>
+              <Typography noWrap variant="body2" color="text.secondary" component="div">
+                {event.location}
+              </Typography>
+            </CardContent>
+            <Box display="flex" padding="4px 8px 4px 16px" sx={{ background: "lightgrey" }}>
+              <Typography noWrap variant="body2" color="text.secondary" component="div">
+                {event.hostName}
+              </Typography>
+            </Box>
+          </CardActionArea>
+        </Card>
+
+      </Grid>)
   }
 
   handleLoadMoreClick = (e: any) => {
-    this.props.fetchEventsAtNextPage()
+    this.furthestPage += 1
+    this.props.fetchEvents(this.furthestPage)
   }
 
   render() {
@@ -57,6 +89,7 @@ class EventsLanding extends React.Component<EventsLandingProps, EventsLandingSta
           display="flex"
           justifyContent="space-between"
           width="100%"
+          pb="16px"
         >
           <h1 style={{ fontWeight: 300, margin: "0px" }}>Events</h1>
           <Button
@@ -67,7 +100,7 @@ class EventsLanding extends React.Component<EventsLandingProps, EventsLandingSta
           > Create
           </Button>
         </Box>
-        {!this.props.isEventsFetched  && <CircularProgress/>}
+        {!this.props.isEventsFetched && <CircularProgress />}
         {this.props.isEventsFetched && this.props.events.length == 0 && <div>No events found</div>}
         {
           this.props.events.length != 0 &&
@@ -76,14 +109,14 @@ class EventsLanding extends React.Component<EventsLandingProps, EventsLandingSta
             id="top-page-container"
             spacing={2}
           >
-            {this.getCards([])}
+            {this.getCards(this.props.events)}
           </Grid>
         }
         {
           this.props.events.length != 0 && !this.props.isLastPage &&
           <Button
             variant="outlined"
-            sx={{ color: "black", border: "1px solid black" }}
+            sx={{ color: "black", border: "1px solid black", margin: "32px 0px" }}
             onClick={this.handleLoadMoreClick}
           >
             Load more
@@ -106,7 +139,7 @@ const mapStateToProps = ((state: RootState) => {
 
 const mapDispatchToProps = ((dispatch: AppDispatch) => {
   return {
-    fetchEventsAtNextPage: () => dispatch(fetchEventsAtNextPage()),
+    fetchEvents: (furthestPage: number) => dispatch(fetchEvents(furthestPage)),
   }
 })
 
