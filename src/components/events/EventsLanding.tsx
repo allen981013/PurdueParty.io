6176454a8a7a80b07a5d8ceba20463c5dfe14173
micmag33 <84053501+micmag33@users.noreply.github.com>
2021-10-06 17:58:08 -0400

    logout inaccessible criteria

--- src/components/events/EventsLanding.tsx
+++ src/components/events/EventsLanding.tsx
@@ -103,7 +103,8 @@ class EventsLanding extends React.Component<EventsLandingProps, EventsLandingSta
   }
 
   render() {
-
+    const { auth } = this.props;
+    if (!auth) return <Redirect to='/signin' />
     return (
       <Box
         display="flex"
